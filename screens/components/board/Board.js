d88631347e9ccca56c1dba4daf1c93b75afd1c36
JON Washington <jonwashPro@gmail.com>
2025-12-20 21:41:52 -0600

    Improvements to board in regards to handling modes & event listeners

--- screens/components/board/Board.js
+++ screens/components/board/Board.js
@@ -10,7 +10,6 @@ export default class Board {
 		this.follow = options?.follow || true;
 		this.enableTargeting = options?.enableTargeting || false;
 		// this.#hoverCells = [];
-		this.shipToPlace = '';
 		// Board.boardNumber++;
 		console.log("Create Board number", Board.boardNumber);
 	};
@@ -30,47 +29,49 @@ export default class Board {
 	#hoverCells = [];
 	#activeCells = [];
 	#pointerCell;
+	#shipToPlace;
+	#donePlacingShip;
 	
 
 	async render() { new Promise((resolve,reject) => {
 		this.root.innerHTML = `
-		<section class="targeting-system${Board.boardNumber}">
-			<div class="crosshairs${Board.boardNumber} xhv${Board.boardNumber} hide${Board.boardNumber}"></div>
-			<div class="crosshairs${Board.boardNumber} xhh${Board.boardNumber} hide${Board.boardNumber}"></div>
-			<svg class="targeting-disc${Board.boardNumber} hide${Board.boardNumber}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 157.06 155.27"><defs><style>.cls-1{fill:none;stroke:#f9c31b;stroke-miterlimit:10;stroke-width:4px;}.cls-2{fill:aqua;}</style></defs><g id="Layer_1-2">
+		<section class="targeting-system">
+			<div class="crosshairs xhv hide"></div>
+			<div class="crosshairs xhh hide"></div>
+			<svg class="targeting-disc hide" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 157.06 155.27"><defs><style>.cls-1{fill:none;stroke:#f9c31b;stroke-miterlimit:10;stroke-width:4px;}.cls-2{fill:aqua;}</style></defs><g id="Layer_1-2">
 				<path class="cls-2" d="M128.76,77.63c0-6.89-1.39-13.45-3.91-19.42,0-.02-.01-.03-.02-.05-2.57-6.06-6.3-11.52-10.91-16.08L151.35,5.05l4.42,4.89c1.46,1.61,1.71,3.97.62,5.85-5.11,8.86-19.24,33.32-19.24,33.32,4.23,8.62,6.61,18.3,6.61,28.52,0,10.23-2.38,19.91-6.61,28.53l19.24,33.32c1.09,1.88.83,4.25-.62,5.86l-4.42,4.89-37.43-37.03c4.61-4.56,8.34-10.01,10.91-16.08,0-.02.01-.03.02-.05,2.52-5.97,3.91-12.54,3.91-19.42M144.21,113.38c5.82-10.62,9.14-22.8,9.14-35.74s-3.31-25.12-9.13-35.74l-2.38,4.06c4.81,9.53,7.51,20.29,7.51,31.68s-2.7,22.13-7.5,31.65l2.36,4.1ZM28.3,77.54c0,6.89,1.39,13.45,3.91,19.42,0,.02.01.03.02.05,2.57,6.06,6.3,11.52,10.91,16.08L5.72,150.12l-4.42-4.89c-1.46-1.61-1.71-3.98-.62-5.86l19.24-33.32c-4.22-8.62-6.61-18.3-6.61-28.53,0-10.23,2.38-19.91,6.61-28.52,0,0-14.13-24.47-19.24-33.32-1.09-1.88-.83-4.24.62-5.85l4.42-4.89,37.43,37.03c-4.61,4.56-8.34,10.01-10.91,16.08,0,.02-.01.03-.02.05-2.52,5.97-3.91,12.54-3.91,19.42M15.22,109.19c-4.8-9.52-7.5-20.28-7.5-31.65s2.71-22.14,7.51-31.68l-2.38-4.06c-5.82,10.62-9.13,22.8-9.13,35.74s3.31,25.12,9.14,35.74l2.36-4.1Z"/><path class="cls-1" d="M147.59,1.47l-32.8,30.12c-9.93-7.78-22.44-12.42-36.03-12.42-13.59,0-26.1,4.64-36.03,12.42,0,0-33.26-29.7-33.26-29.7M148.05,153.37s-33.26-29.7-33.26-29.7c-9.93,7.78-22.44,12.42-36.03,12.42s-26.1-4.64-36.03-12.42l-32.8,30.12M78.76,27.63c-27.61,0-50,22.39-50,50s22.39,50,50,50,50-22.39,50-50-22.39-50-50-50Z"/>
 			</g></svg>
-			<div class="targeting-brackets${Board.boardNumber} hide${Board.boardNumber}">
-				<div class="bracket-node${Board.boardNumber}"><span></span></div>
+			<div class="targeting-brackets hide">
+				<div class="bracket-node"><span></span></div>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -107 1014.17 1014.17"><g>
 					<path d="M800,769.2h106.91c5.55,0,10.04-4.5,10.04-10.04v-49.55h97.21v80.35c0,5.55-4.5,10.04-10.04,10.04h-89.17s-114.95,0-114.95,0v-30.8ZM800,30.8h106.91c5.55,0,10.04,4.5,10.04,10.04v49.55h97.21V10.04c0-5.55-4.5-10.04-10.04-10.04h-89.17s-114.95,0-114.95,0v30.8ZM990.65,107.45h-75.7v585.09h75.7V107.45ZM214.17,800H10.04c-5.55,0-10.04-4.5-10.04-10.04v-80.35h97.21s0,49.55,0,49.55c0,5.55,4.5,10.04,10.04,10.04h106.91v30.8ZM214.17,0h-114.95s-89.17,0-89.17,0C4.5,0,0,4.5,0,10.04v80.35s97.21,0,97.21,0v-49.55c0-5.55,4.5-10.04,10.04-10.04h106.91s0-30.8,0-30.8ZM23.52,692.55h75.7s0-585.09,0-585.09H23.52s0,585.09,0,585.09Z"/>
 				</g></svg>
 			</div>
 		</section>
-		<div class="Board${Board.boardNumber}">
+		<div class="Board">
 			<header></header>
-			<aside class="left${Board.boardNumber}" style="grid-area:m-rows-l;"></aside>
-			<section class="Board-grid-container${Board.boardNumber}"></section>
-			<aside class="right${Board.boardNumber}" style="grid-area:m-rows-r;"></aside>
+			<aside class="left" style="grid-area:m-rows-l;"></aside>
+			<section class="Board-grid-container"></section>
+			<aside class="right" style="grid-area:m-rows-r;"></aside>
 			<footer></footer>
 		</div>`;
 
 		// INIT
-		this.target = this.root.querySelector(`.Board${Board.boardNumber}`);
-		this.container = this.root.querySelector(`.Board-grid-container${Board.boardNumber}`);
-		this.header = this.root.querySelector(`.Board${Board.boardNumber} header`);
-		this.footer = this.root.querySelector(`.Board${Board.boardNumber} footer`);
-		this.left = this.root.querySelector(`.Board${Board.boardNumber} aside.left${Board.boardNumber}`);
-		this.right = this.root.querySelector(`.Board${Board.boardNumber} aside.right${Board.boardNumber}`);
+		this.target = this.root.querySelector(`.Board`);
+		this.container = this.root.querySelector(`.Board-grid-container`);
+		this.header = this.root.querySelector(`.Board header`);
+		this.footer = this.root.querySelector(`.Board footer`);
+		this.left = this.root.querySelector(`.Board aside.left`);
+		this.right = this.root.querySelector(`.Board aside.right`);
 		this.letters = [this.header, this.footer];
 		this.numbers = [this.left, this.right];
-		this.targeting = this.root.querySelector(`section.targeting-system${Board.boardNumber}`);
-		this.brackets = this.root.querySelector(`.targeting-brackets${Board.boardNumber}`);
-		this.disc = this.root.querySelector(`.targeting-disc${Board.boardNumber}`);
-		this.crosshairs = this.root.querySelectorAll(`.crosshairs${Board.boardNumber}`);
-		this.crosshairV = this.root.querySelector(`.xhv${Board.boardNumber}`);
-		this.crosshairH = this.root.querySelector(`.xhh${Board.boardNumber}`);
-		this.bracketNode = this.root.querySelector(`.bracket-node${Board.boardNumber} > span`);
+		this.targeting = this.root.querySelector(`section.targeting-system`);
+		this.brackets = this.root.querySelector(`.targeting-brackets`);
+		this.disc = this.root.querySelector(`.targeting-disc`);
+		this.crosshairs = this.root.querySelectorAll(`.crosshairs`);
+		this.crosshairV = this.root.querySelector(`.xhv`);
+		this.crosshairH = this.root.querySelector(`.xhh`);
+		this.bracketNode = this.root.querySelector(`.bracket-node > span`);
 		
 		this.createBoard();
 		document.addEventListener('keypress', this.handleKeyPress.bind(this));
@@ -78,13 +79,14 @@ export default class Board {
 
 		this.brackets['xpos'] = window.innerWidth / 2;
 		this.brackets['ypos'] = window.innerHeight / 2;
-		this.enableTargeting && this.brackets.classList.toggle(`hide${Board.boardNumber}`);
-		this.disc['xpos'] = window.innerWidth / 2;
+		// this.bracketNode.innerHTML = `${this.brackets.xpos},${this.brackets.ypos}`;
+		this.enableTargeting && this.brackets.classList.toggle('hide');
+		this.disc['xpos'] = window.innerWidth / 2; 
 		this.disc['ypos'] = window.innerHeight / 2;
-		this.enableTargeting && this.disc.classList.toggle(`hide${Board.boardNumber}`);
+		this.enableTargeting && this.disc.classList.toggle('hide');
 		this.crosshairV['xpos'] = window.innerWidth / 2;
 		this.crosshairH['ypos'] = window.innerHeight / 2;
-		this.enableTargeting && this.crosshairs.forEach(el=>el.classList.toggle(`hide${Board.boardNumber}`));
+		this.enableTargeting && this.crosshairs.forEach(el=>el.classList.toggle('hide'));
 
 		this.update();
 		resolve(console.log("done.", Board.boardNumber)) }); 
@@ -105,13 +107,13 @@ export default class Board {
 			});
 
 			const row = document.createElement('div');
-			row.classList.add(`board-row${Board.boardNumber}`);
+			row.classList.add(`board-row`);
 			Board.colRow.forEach((c, k) => {
 				const el = document.createElement('div');
 				let key = `${String(i + 1).padStart(2, '0')}${String(k + 1).padStart(2, '0')}`;
 				el.id = key;
 				el.classList.add(`${Board.colRow[i][0]}${c[1]}`);
-				el.classList.add(`cell${Board.boardNumber}`);
+				el.classList.add('cell', 'hover-cell');
 				row.appendChild(el);
 				this.cells[key] = el;
 			});
@@ -121,13 +123,18 @@ export default class Board {
 
     // METHODS
     mode(option) {
-        this.status.mode = option;
+        option && (this.status.mode = option);
         switch (option) {
             case 'place-ships': {
 				this.target.addEventListener('click', this.placeShip.bind(this));
+				this.brackets.classList.remove('hide');
+				this.target.querySelectorAll('.board-row div').forEach(el=>el.classList.remove('.hover-cell'));
 			} break;
+			case 'targeting': {this.follow = true; this.disc.classList.remove('hide');this.crosshairs.forEach(el=>el.classList.remove('hide'))} break;
 			case 'static': {this.target.removeEventListener('click', this.placeShip.bind(this))} break;
+			default: {return this.status.mode}
         }
+
     }
 
 	// EVENT HANDLERS
@@ -135,21 +142,21 @@ export default class Board {
 		const hoverOver = document.elementsFromPoint(e.clientX,e.clientY);
 		if (this.follow && hoverOver.includes(this.target)) {
 			this.#pointerCell ?? (this.#pointerCell = hoverOver[0]);
-			this.targeting.classList.remove(`hide${Board.boardNumber}`);
-			this.targeting.classList.add(`show${Board.boardNumber}`);
+			this.targeting.classList.remove('hide');
+			this.targeting.classList.add('show');
 			this.disc.xpos = e.clientX;
 			this.disc.ypos = e.clientY;
 			this.crosshairV.xpos = e.clientX;
 			this.crosshairH.ypos = e.clientY;
-			this.bracketNode.textContent = hoverOver[0].classList;
-			if (this.status.mode === 'place-ships') {
-				(hoverOver[0]!==this.#pointerCell) && this.hoverSillhouette(hoverOver);
+			this.bracketNode.innerHTML = hoverOver[0].classList[0]; /*`M:${e.clientX},${e.clientY}<br>B:${this.brackets.xpos},${this.brackets.ypos}`;*/
+			if (this.mode() === 'place-ships') {
+				if (this.#shipToPlace && hoverOver[0]!==this.#pointerCell) this.hoverSillhouette(hoverOver);
 				this.brackets.xpos = remap(e.clientX, 0, window.innerWidth, window.innerWidth*0.10, window.innerWidth*0.9);
-				this.brackets.ypos = remap(e.clientY, 0, window.innerHeight, window.innerHeight*0.1, window.innerHeight*0.90);
+				this.brackets.ypos = remap(e.clientY, -150, window.innerHeight, window.innerHeight*0.1, window.innerHeight*0.90);
 			}
 		} else if (!hoverOver.includes(Board)) {
-			this.targeting.classList.remove(`show${Board.boardNumber}`);
-			this.targeting.classList.add(`hide${Board.boardNumber}`);
+			this.targeting.classList.remove('show');
+			this.targeting.classList.add('hide');
 			this.cleanCellResidue && this.doCleanCellResidue();
 		}
 		this.update();
@@ -159,9 +166,9 @@ export default class Board {
     	e.preventDefault();
     	switch (e.key) {
 			case ' ': {this.follow = !this.follow} break;
-			case 'c': {this.crosshairs.forEach(el=>el.classList.toggle(`hide${Board.boardNumber}`))} break;
-			case 'b': {this.brackets.classList.toggle(`hide${Board.boardNumber}`)} break;
-			case 'd': {this.disc.classList.toggle(`hide${Board.boardNumber}`)} break;
+			case 'c': {this.crosshairs.forEach(el=>el.classList.toggle('hide'))} break;
+			case 'b': {this.brackets.classList.toggle('hide')} break;
+			case 'd': {this.disc.classList.toggle('hide')} break;
 		}
 	}
 
@@ -169,12 +176,12 @@ export default class Board {
 		this.#pointerCell = hoverOver[0];
 		this.cleanCellResidue = true;
 		this.#hoverCells.forEach(h=>{ 
-			h.classList.remove(`hoverCell${Board.boardNumber}`); 
-			h.classList.remove(`impededCell${Board.boardNumber}`) 
+			h.classList.remove(`hoverCell`); 
+			h.classList.remove(`impededCell`) 
 		});
-		this.#activeCells.forEach(a=>a.classList.remove(`impededCell${Board.boardNumber}`));
+		this.#activeCells.forEach(a=>a.classList.remove(`impededCell`));
 		this.#hoverCells = [];
-		const ship = this.shipToPlace;console.log("this.shipToPlace")
+		const ship = this.#shipToPlace;
 		let cell = hoverOver[0].id;
 		ship.sillhouette.forEach((row,i)=>{
 			row.forEach((c,k)=>{
@@ -182,30 +189,36 @@ export default class Board {
 				if (c==1) {
 					const alter = this.cells[idx(Number(cell))];
 					try {
-					if (alter.classList.contains(`committedCell${Board.boardNumber}`)) {
-						alter.classList.add(`impededCell${Board.boardNumber}`)
+					if (alter.classList.contains(`committedCell`)) {
+						alter.classList.add(`impededCell`)
 					} else {
-						alter.classList.add(`hoverCell${Board.boardNumber}`);
+						alter.classList.add(`hoverCell`);
 						this.#hoverCells.push(alter);
-					}} catch (err) {this.#hoverCells.forEach(c=>c.classList.add(`impededCell${Board.boardNumber}`));}
+					}} catch (err) {this.#hoverCells.forEach(c=>c.classList.add(`impededCell`));}
 				};
 			})
 		});
 	}
 
+	setShipToPlace(ship,resolve) {this.#shipToPlace = ship; 
+		console.log("ship recieved:", ship);
+		this.#donePlacingShip = resolve;
+	};
+
     placeShip(e) {
-		let ship = this.shipToPlace;
+		if (!this.#shipToPlace) return;
+		let ship = this.#shipToPlace;
 		const stagedCells = [];
 		let alter;
-		if (e.target.classList.contains(`cell${Board.boardNumber}`)) {
+		if (e.target.classList.contains(`cell`)) {
             const cell = e.target.id;
             ship.sillhouette.forEach((row,i)=>{
                 row.forEach((c,k)=>{
                     const idx = (num) => String( num + ((100 * i) + (k - ship.offset())) ).padStart(4,'0'); // INCREMENT 4 DIGIT CELL KEY FROM CLICKED
                     if (c==1) {
 						alter = this.cells[idx(Number(cell))];
-						if (alter.classList.contains(`committedCell${Board.boardNumber}`)) {
-							alter.classList.add(`forbiddenCell${Board.boardNumber}`)
+						if (alter.classList.contains(`committedCell`)) {
+							alter.classList.add(`forbiddenCell`)
 							stagedCells.push(alter);
 						} else {
 							console.log("+active", alter); //!
@@ -215,35 +228,35 @@ export default class Board {
                 })
             })
 		};
-		if (stagedCells.every(c=>!c.classList.contains(`forbiddenCell${Board.boardNumber}`))) {
-			stagedCells.forEach(c=>c.classList.add(`committedCell${Board.boardNumber}`));
+		if (stagedCells.every(c=>!c.classList.contains(`forbiddenCell`))) {
+			stagedCells.forEach(c=>c.classList.add(`committedCell`));
 			console.log("Cells Pushed!", stagedCells); //!
 			this.#activeCells.push(...stagedCells); 
-			this.shipToPlace[location] = [e.target.id,stagedCells];
-			this.placedShips[this.shipToPlace.name] = this.shipToPlace;
-		} else {console.log("Cells Rejected!",stagedCells.some(c=>c.classList.contains(`forbiddenCell${Board.boardNumber}`)))}; //!
+			this.#shipToPlace[location] = [e.target.id,stagedCells];
+			this.placedShips[this.#shipToPlace.name] = this.#shipToPlace;
+		} else {console.log("Cells Rejected!",stagedCells.some(c=>c.classList.contains(`forbiddenCell`)))}; //!
 		stagedCells.forEach(c=>{
-			c.classList.remove(`forbiddenCell${Board.boardNumber}`);
-			c.classList.remove(`impededCell${Board.boardNumber}`);
-			c.classList.remove(`hoverCell${Board.boardNumber}`)
+			c.classList.remove(`forbiddenCell`);
+			c.classList.remove(`impededCell`);
+			c.classList.remove(`hoverCell`)
 		});
+		this.#shipToPlace = null;
+		this.#donePlacingShip();
 	}
-
-	setShipToPlace(ship) {this.shipToPlace = ship; console.log("board recieved:", ship)};
 
     update() {
         this.brackets.style.left = this.brackets.xpos + 'px';
         this.brackets.style.top = this.brackets.ypos + 'px';
         this.disc.style.left = this.disc.xpos + 'px';
         this.disc.style.top = this.disc.ypos + 'px';
-        this.crosshairV.style.left = this.disc.xpos + 'px';
-        this.crosshairH.style.top = this.disc.ypos + 'px';
+        this.crosshairV.style.left = this.crosshairV.xpos + 'px';
+        this.crosshairH.style.top = this.crosshairH.ypos + 'px';
     }
 
 	cleanCellResidue = false;
 	doCleanCellResidue() { Object.values(this.cells).forEach(c => {
-		c.classList.contains(`impededCell${Board.boardNumber}`) && c.classList.remove(`impededCell${Board.boardNumber}`);
-		c.classList.contains(`hoverCell${Board.boardNumber}`) && c.classList.remove(`hoverCell${Board.boardNumber}`);
+		c.classList.contains(`impededCell`) && c.classList.remove(`impededCell`);
+		c.classList.contains(`hoverCell`) && c.classList.remove(`hoverCell`);
 		this.cleanCellResidue = false;
 	})};
 };
--- screens/dashboard/dashboard.js
+++ screens/dashboard/dashboard.js
@@ -7,87 +7,92 @@ import { nav } from "../../nav_dev.js";nav();
 const targetingSystem = document.getElementById('load-targeting-System');
 let activeProjection;
 
-const dashboard = {
-    p1: {
-        boardEl: document.getElementById('p1-board'),
-        gameSummaryPanel: document.getElementById('p1-summary-text-overlay'),
-        shipsPanel: document.getElementById('p1-ships-text-overlay'),
-        statusPanel: document.getElementById('p1-status-text-overlay'),
-        placeShips(e) {
-            if (e.target.classList.contains('ship')) {
-                const projection = dashboard.projections[e.target.id];
-                if (!activeProjection) {
-                    activeProjection = projection;
-                    projection.addEventListener('mousemove',handleMouseMove);
-                    projection.classList.replace('hide','show');
-                    projection.classList.add('project');
-                } else {
-                    activeProjection.classList.replace('show','hide');
-                    activeProjection.removeEventListener('mousemove',handleMouseMove);
-                    activeProjection.classList.remove('project');
-                    activeProjection = null;
-                } 
-            }
+
+const p1 = {
+    boardEl: document.getElementById('p1-board'),
+    gameSummaryPanel: document.getElementById('p1-summary-text-overlay'),
+    shipsPanel: document.getElementById('p1-ships-text-overlay'),
+    statusPanel: document.getElementById('p1-status-text-overlay'),
+    placeShips(e) {
+        if (e.target.classList.contains('ship')) {
+            const projection = this.projections[e.target.id];
+            if (!activeProjection) {
+                activeProjection = projection;
+                projection.addEventListener('mousemove',handleMouseMove);
+                projection.classList.replace('hide','show');
+                projection.classList.add('project');
+            } else {
+                activeProjection.classList.replace('show','hide');
+                activeProjection.removeEventListener('mousemove',handleMouseMove);
+                activeProjection.classList.remove('project');
+                activeProjection = null;
+            } 
         }
-    },
-    p2: {
-        boardEl: document.getElementById('p2-board'),
-        gameSummaryPanel: document.getElementById('p2-summary-text-overlay'),
-        shipsPanel: document.getElementById('p2-ships-text-overlay'),
-        statusPanel: document.getElementById('p2-status-text-overlay'),
-        ships: {
-            enterprise: document.querySelector('#enterprise'),
-            titan: document.querySelector('#titan'),
-            wallenberg: document.querySelector('#wallenberg'),
-            lasirena: document.querySelector('#lasirena'),
-            shuttle: document.querySelector('#shuttle')
-        },
-        projections: {
-            enterprise: document.querySelector('#enterprise-projection'),
-            titan: document.querySelector('#titan-projection'),
-            wallenberg: document.querySelector('#wallenberg-projection'),
-            lasirena: document.querySelector('#lasirena-projection'),
-            shuttle: document.querySelector('#shuttle-projection')
-        },
-        placeShips(e) {
-            if (e.target.classList.contains('ship')) {
-                console.log(ships.starfleet[e.target.id]); //!
-                const projection = dashboard.p2.projections[e.target.id];
-                if (!activeProjection) {
-                    activeProjection = projection;
-                    projection.addEventListener('mousemove',handleMouseMove);
-                    projection.classList.replace('hide','show');
-                    projection.classList.add('project');
-                } else {
-                    activeProjection.classList.replace('show','hide');
-                    activeProjection.removeEventListener('mousemove',handleMouseMove);
-                    activeProjection.classList.remove('project');
-                    activeProjection = null;
-                }
-                // this.board.setShipToPlace(player2.ships[e.target.id]);
-                // const send = ships.starfleet[e.target.id];
-                dashboard.p2.board.setShipToPlace(ships.starfleet[e.target.id]);
-            }
-        }
-    },
-    buttons: {
-        fire: document.getElementById('btn-fire'),
-        focusPlayer: document.getElementById('btn-focus-player'),
-        focusEnemy: document.getElementById('btn-focus-enemy'),
-        fullBoard: document.getElementById('btn-full-board'),
-        pause: document.getElementById('btn-pause'),
-        orient: document.getElementById('btn-orient'),
-        switchUser: document.getElementById('btn-switch-user'),
-        turnTimer: {
-            target: document.getElementById('turn-timer'),
-            start() {
-                this.reset();
-                this.target.textContent = config.turnTimer;
-            },
-            reset() {this.target.textContent = '00:00.00'},
-        },
     }
 }
+const allShips = Object.values(ships.starfleet);
+const iterator = 0;
+
+const p2 = {
+    boardEl: document.getElementById('p2-board'),
+    gameSummaryPanel: document.getElementById('p2-summary-text-overlay'),
+    shipsPanel: document.getElementById('p2-ships-text-overlay'),
+    statusPanel: document.getElementById('p2-status-text-overlay'),
+    board:null,
+    ships: {
+        enterprise: document.querySelector('#enterprise'),
+        titan: document.querySelector('#titan'),
+        wallenberg: document.querySelector('#wallenberg'),
+        lasirena: document.querySelector('#lasirena'),
+        shuttle: document.querySelector('#shuttle')
+    },
+    projections: {
+        enterprise: document.querySelector('#enterprise-projection'),
+        titan: document.querySelector('#titan-projection'),
+        wallenberg: document.querySelector('#wallenberg-projection'),
+        lasirena: document.querySelector('#lasirena-projection'),
+        shuttle: document.querySelector('#shuttle-projection')
+    },
+    placeShips(e) {
+        if (e.target.classList.contains('ship')) {
+            console.log(ships.starfleet[e.target.id]); //!
+            const projection = p2.projections[e.target.id];
+            if (!activeProjection) {
+                activeProjection = projection;
+                projection.addEventListener('mousemove',handleMouseMove);
+                projection.classList.replace('hide','show');
+                projection.classList.add('project');
+            } else {
+                activeProjection.classList.replace('show','hide');
+                activeProjection.removeEventListener('mousemove',handleMouseMove);
+                activeProjection.classList.remove('project');
+                activeProjection = null;
+            }
+            // this.board.setShipToPlace(player2.ships[e.target.id]);
+            // const send = ships.starfleet[e.target.id];
+            p2.board.setShipToPlace(ships.starfleet[e.target.id]);
+        }
+    },
+}
+
+const buttons = {
+    fire: document.getElementById('btn-fire'),
+    focusPlayer: document.getElementById('btn-focus-player'),
+    focusEnemy: document.getElementById('btn-focus-enemy'),
+    fullBoard: document.getElementById('btn-full-board'),
+    pause: document.getElementById('btn-pause'),
+    orient: document.getElementById('btn-orient'),
+    switchUser: document.getElementById('btn-switch-user'),
+    turnTimer: {
+        target: document.getElementById('turn-timer'),
+        start() {
+            this.reset();
+            this.target.textContent = config.turnTimer;
+        },
+        reset() {this.target.textContent = '00:00.00'},
+    },
+}
+
 
 function handleMouseMove(e) {
     const hoverOver = document.elementsFromPoint(e.clientX,e.clientY);
@@ -114,27 +119,39 @@ function handleMouseMove(e) {
 }
 
 async function init() {
-    dashboard.p1['board'] = new Board(dashboard.p1.boardEl,targetingSystem,{follow:false,enableTargeting:false});
-    await dashboard.p1.board.render();
-    dashboard.p2['board'] = new Board(dashboard.p2.boardEl,targetingSystem,{follow:false,enableTargeting:false});
-    await dashboard.p2.board.render();
+    p1['board'] = new Board(p1.boardEl,targetingSystem,{follow:false,enableTargeting:false});
+    await p1.board.render();
+    p2['board'] = new Board(p2.boardEl,targetingSystem,{follow:false,enableTargeting:false});
+    await p2.board.render();
     
-    dashboard.buttons.switchUser.addEventListener('click',()=>p2.board.mode('static'));
-    dashboard.buttons.switchUser.textContent = 'done';
-    dashboard.buttons.orient.addEventListener('click', (e)=>dashboard.p2.boardEl.classList.toggle('rotate'));
-    dashboard.buttons.orient.classList.add('clickable');
-    dashboard.buttons.switchUser.classList.add('clickable');
+    buttons.switchUser.addEventListener('click',()=>p2.board.mode('static'));
+    buttons.switchUser.textContent = 'done';
+    buttons.orient.addEventListener('click', (e)=>p2.boardEl.classList.toggle('rotate'));
+    buttons.orient.classList.add('clickable');
+    buttons.switchUser.classList.add('clickable');
 
-    dashboard.p2.board.mode('place-ships');
-    dashboard.p2.shipsPanel.addEventListener('click', dashboard.p2.placeShips); //!
-    Object.values(dashboard.p2.ships).forEach(s=>s.classList.add('clickable'));
+    p2.board.mode('place-ships');
+    p2.shipsPanel.addEventListener('click', p2.placeShips); //!
+    Object.values(p2.ships).forEach(s=>s.classList.add('clickable'));
 
     document.addEventListener('mousemove',(e)=>{
         const elements = document.elementsFromPoint(e.clientX,e.clientY);
         // console.log(elements)//!
-        if (elements.includes(dashboard.p2.shipsPanel)) {
+        if (elements.includes(p2.shipsPanel)) {
             document.querySelector('main').style.pointerEvents = 'none';
         } else {document.querySelector('main').style.pointerEvents = 'all';}
     });
 
+    // p2.board.brackets.xpos = window.innerWidth/2;
+    // p2.board.brackets.ypos = window.innerHeight/2;
+    p2.board.mode('place-ships')
+    p2.board.update()
+    console.log(p2.board.mode());
+
 }; await init();
+
+function testSend() {
+    		new Promise((resolve) => {
+            console.log(p2.board.setShipToPlace(ships.starfleet.lasirena,resolve))
+		})
+} testSend()
